6b4c62a50a8bdaf5a34aac3bf703f9629d337b11
Javier Librandi <javierlibrandi@gmail.com>
2013-05-02 01:07:21 -0300

    Refactorizo el codigo para que se mas claro. Pero no modifico funcionalidad.

--- plataforma/src/planificador/planificador_thr.c
+++ plataforma/src/planificador/planificador_thr.c
@@ -6,25 +6,31 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
+#include <mario_para_todos/ambiente_config.h>
 #include <commons/config.h>
 #include <mario_para_todos/grabar.h>
 #include <mario_para_todos/comunicacion/Socket_Servidor.h>
 #include <mario_para_todos/comunicacion/Socket.h>
+#include <commons/collections/list.h>
+#include "planificador_thr.h"
 
-void *planificador_nivel_thr(void *p) {
-	char *des_nivel = (char *) p;
+void* planificador_nivel_thr(void *p) {
+	t_h_planificador *h_planificador = (t_h_planificador *) p;
 	int sck_server, new_fd, enviados;
-	char msj[1024], puerto[8],rec[5];
+	char msj[1024], rec[5];
+	struct t_param_plan param_planificador; //si no declaro la variable como  "struct t_param_plan" tengo problemas para resolver el nombre
+	char *des_nivel;
 
-
+	des_nivel = h_planificador->desc_nivel;
 
 	log_in_disk_plan(LOG_LEVEL_DEBUG, "Creo el panificador del nivel %s ",
-			des_nivel);
+				des_nivel);
 
+	//leo el archivo de configuracion para el hilo orquestador
+	param_planificador = leer_archivo_plan_config(des_nivel);
+	h_planificador->puerto = param_planificador.PUERTO;
 
-	strcpy(puerto,"5021");
-
-	if ((sck_server = Abre_Socket_Inet(puerto)) == -1) {
+	if ((sck_server = Abre_Socket_Inet(param_planificador.PUERTO)) == -1) {
 
 		log_in_disk_plan(LOG_LEVEL_ERROR,
 				"Error al abrir la conexion para el planificador de nivel",
@@ -34,28 +40,38 @@ void *planificador_nivel_thr(void *p) {
 
 	log_in_disk_plan(LOG_LEVEL_TRACE, "servidor escuchando %s", des_nivel);
 
+	while(1){
+	if ((new_fd = Acepta_Conexion_Cliente(sck_server)) == -1) {
+		log_in_disk_plan(LOG_LEVEL_ERROR, "error en Acepta_Conexion_Cliente %s",
+				des_nivel);
+		exit(1);
+	}
+
 	while (1) {
 
-		if ((new_fd = Acepta_Conexion_Cliente(sck_server)) == -1) {
-			log_in_disk_plan(LOG_LEVEL_ERROR,
-					"error en Acepta_Conexion_Cliente %s", des_nivel);
+
+		strcpy(msj, "Hello, world!\n");
+		enviados = Escribe_Socket(new_fd, msj, strlen(msj));
+		if (enviados == -1) {
+			perror("send");
+			log_in_disk_plan(LOG_LEVEL_ERROR, "error en send %s", des_nivel);
 			exit(1);
 		}
-		strcpy(msj,"Hello, world!\n");
-		enviados=Escribe_Socket(new_fd, msj, strlen(msj));
-		if (enviados == -1){
-				perror("send");
-				log_in_disk_plan(LOG_LEVEL_ERROR,
-									"error en send %s", des_nivel);
-				exit (1);
-			}
 
+		enviados = Lee_Socket(new_fd, rec, 5);
+		log_in_disk_plan(LOG_LEVEL_TRACE, "mensaje recivido %s de un len %d \n ",
+				rec, enviados);
 
-		log_in_disk_plan(LOG_LEVEL_TRACE, "mensaje enviado %s de un len %d ", des_nivel,enviados);
-		enviados=Lee_Socket(new_fd, rec, 5);
-		log_in_disk_plan(LOG_LEVEL_TRACE, "mensaje recivido %s de un len %d ", rec,enviados);
-
+		if (strncmp(rec,"fin",3)==0){
+			close(new_fd);//cierro el socket con el que estoy hablando
+			break; //salgo del segundo while para atender otra conexion
+		}
 	}
+	}
+	//cierro el socket que escucha para no aceptar nuevas conexiones.
+	//Como estoy en un while infinito no tiene sentido lo pogo como ejempo
+	close(sck_server);
 
 	return 0;
 }
+
--- plataforma/src/plataforma.c
+++ plataforma/src/plataforma.c
@@ -18,18 +18,18 @@
 
 void libero_memoria(t_list *list_plataforma);
 
-
+t_list *menejo_hilos_planificador(t_h_planificador *h_planificador, t_param_plat param_plataforma);
 
 
 
 int main(void) {
 
 	t_param_plat param_plataforma;
-	pthread_t orquestador_thr, planificador_thr;
+	pthread_t orquestador_thr;
 	t_h_planificador *h_planificador;
 	t_list *list_plataforma;
-	int index;
-	int puerto_plani;
+
+
 
 
 	//leo el archivo de configuracion para el hilo orquestador
@@ -41,42 +41,51 @@ int main(void) {
 	 */
 	pthread_create(&orquestador_thr, NULL, (void *) orequestador_thr, NULL );
 
-	list_plataforma = list_create(); //creo lista de hilos
-	for (index = 0; param_plataforma.planificador_nivel[index] != '\0'; index++) {
-
-
-		h_planificador =  malloc(sizeof(h_planificador)); //recervo la memoria para almacenar el nuevo hilo
-		h_planificador->desc_nivel=param_plataforma.planificador_nivel[index];//agrego la des del nivel
-		/**
-		 * creo los hilos planificador
-		 */
-		pthread_create(&planificador_thr, NULL,(void*) planificador_nivel_thr,
-				h_planificador);
-
-
-		h_planificador->planificador_thr = planificador_thr;
-		//printf("puerto %d \n",h_planificador->puerto);
-		list_add(list_plataforma, h_planificador); //agrego el nuevo hilo a la lista
-
-	}
-
+	list_plataforma = menejo_hilos_planificador(h_planificador,param_plataforma);
 	pthread_join(orquestador_thr, NULL );
 
-	index = 0;
 
-	void _list_elements(t_h_planificador *h_planificador) {
-
-		pthread_join(h_planificador->planificador_thr, NULL );
-
-		index++;
-	}
-
-	list_iterate(list_plataforma, (void*) _list_elements);
 
 	libero_memoria(list_plataforma);
 	list_destroy(list_plataforma);
 
 	return EXIT_SUCCESS;
+}
+
+t_list *menejo_hilos_planificador(t_h_planificador *h_planificador,t_param_plat param_plataforma){
+	t_list *list_plataforma;
+	int index;
+	pthread_t planificador_thr;
+
+	list_plataforma = list_create(); //creo lista de hilos
+		for (index = 0; param_plataforma.planificador_nivel[index] != '\0'; index++) {
+
+
+			h_planificador =  malloc(sizeof(h_planificador)); //recervo la memoria para almacenar el nuevo hilo
+			h_planificador->desc_nivel=param_plataforma.planificador_nivel[index];//agrego la des del nivel
+			/**
+			 * creo los hilos planificador
+			 */
+			pthread_create(&planificador_thr, NULL,(void*) planificador_nivel_thr,
+					(void*)h_planificador);
+
+			h_planificador->planificador_thr = planificador_thr;
+			list_add(list_plataforma, h_planificador); //agrego el nuevo hilo a la lista
+
+		}
+		index = 0;
+
+		void _list_elements(t_h_planificador *h_planificador) {
+
+			pthread_join(h_planificador->planificador_thr, NULL );
+
+			index++;
+		}
+
+		list_iterate(list_plataforma, (void*) _list_elements);
+
+		return list_plataforma;
+
 }
 
 void libero_memoria(t_list *list_plataforma){
