b7d294b66ab212af257f319a1cb706d9c8576c89
Jazmin <jazminacunia@gmail.com>
2013-05-25 16:15:45 -0300

    Se agregan posibles cambios en los mensajes en fileDescriptors como comentarios.

--- mario_para_todos/mario_para_todos/comunicacion/FileDescriptors.h
+++ mario_para_todos/mario_para_todos/comunicacion/FileDescriptors.h
@@ -19,6 +19,40 @@
 #define SALUDO_NIVEL 5
 #define N_TO_O_SALUDO 6
 
+/* JAZ
+ *
+ * #define SALUDO_PERSONAJE 0
+
+//#define PUERTO_PLANIFICADOR 2
+//#define PUETO_NIVEL 3
+//#define NOMBRE_PERSONAJE 4 ??
+//#define SALUDO_NIVEL 5
+
+#define	P_TO_O_PROX_NIVEL 1
+//
+
+#define	O_TO_P_UBIC_NIVEL 2//del tipo InfoProxNivel	//ex PUETO_NIVEL y PUERTO_PLANIFICADOR
+#define P_TO_N_INICIAR_NIVEL 3
+#define	P_TO_PL_INICIAR_NIVEL 4 //ex SALUDO_NIVEL
+#define	P_TO_N_UBIC_RECURSO 5
+#define	N_TO_P_UBIC_RECURSO 6
+#define	PL_TO_P_TURNO 7
+#define	P_TO_PL_TURNO_CUMPLIDO 8
+#define	P_TO_N_SOLIC_RECURSO 9
+#define	P_TO_N_BLOQUEO 10
+#define	P_TO_N_MUERTE 11
+#define	P_TO_N_OBJ_CUMPLIDO 12
+#define	P_TO_N_REINICIAR_NIVEL 13
+#define	P_TO_O_REINICIAR_JUEGO 14
+
+//SALUDO_PERSONAJE: primer mensaje a orquestador solo para identificarse como personaje
+//P_TO_O_PROX_NIVEL: mensaje que envía el personaje al orquestador para solicitar ip/puerto de su proximo nivel y planif
+//O_TO_P_UBIC_NIVEL: respuesta del orquestador con ip/puertos solicitados
+//P_TO_N_INICIAR_NIVEL: mensaje que envía el personaje al nivel para conectarse al mismo y ubicarse en el mapa
+//P_TO_PL_INICIAR_NIVEL: mensaje que envía el personaje al planificador asociado al nivel para comenzar a esperar su turno para jugar
+
+ */
+
 
 #define OK 100
 #define ERROR 101
