6e78b4655eb0d17195997e4d6f5329ea1033d88e
Juan Matias Chekherdemian <cheker85@gmail.com>
2013-06-07 23:36:49 -0300

    merge

--- plataforma/src/planificador/planificador_thr.c
+++ plataforma/src/planificador/planificador_thr.c
@@ -17,49 +17,90 @@
 #include "planificador_thr.h"
 #include <commons/collections/list.h>
 #include <mario_para_todos/comunicacion/FileDescriptors.h>
+#include <mario_para_todos/entorno.h>
+#include <pthread.h>
+#include <mario_para_todos/entorno.h>
+
+static void eliminar_nodo(int sck,  t_list *list_planifidores);
 
 void* planificador_nivel_thr(void *p) {
 	t_h_planificador *h_planificador = (t_h_planificador *) p;
 
 	void *buffer = NULL;
-	int tipo;
-	int i;
+	int tipo,sck;
+
+//	char *hostname;
+//	int size;
 
 	log_in_disk_plan(LOG_LEVEL_DEBUG, "Creo el panificador del nivel %s ",
 			h_planificador->desc_nivel);
 
-	h_planificador->exceptfds = h_planificador->readfds;
-
 	for (;;) {
 		if (select(*(h_planificador->sock) + 1, h_planificador->readfds, NULL,
-				h_planificador->exceptfds, NULL ) == -1) {
+				NULL, NULL ) == -1) {
 			perror("select");
 			exit(EXIT_FAILURE);
 		}
 
-		for (i = 0; i <= *(h_planificador->sock); i++) {
+		for (sck = 0; sck <= *(h_planificador->sock); sck++) {
 
-			if (FD_ISSET(i, h_planificador->exceptfds)) {
-				log_in_disk_plan(LOG_LEVEL_ERROR,
-						"Error en el sockect.... lo saco de la lista  ");
-				FD_CLR(i, h_planificador->exceptfds);
-				continue;
-			}
+			if (FD_ISSET(sck, h_planificador->readfds)) {
+				buffer = recv_variable(sck, &tipo);
+				if (!strcmp(buffer, Leido_error)) {
 
-			if (FD_ISSET(i, h_planificador->readfds)) {
-				buffer = recv_variable(*(h_planificador->sock), &tipo);
-				log_in_disk_plan(LOG_LEVEL_TRACE,
-						"salida del segundo llamado %s nivel %s \n",
-						(char*) buffer, h_planificador->desc_nivel);
+					elimino_sck_lista(sck, h_planificador->readfds);
 
+					pthread_mutex_lock(h_planificador->s_lista_plani);
+					eliminar_nodo(sck,h_planificador->lista_planificadores);
+					pthread_mutex_unlock(h_planificador->s_lista_plani);
+
+					log_in_disk_plan(LOG_LEVEL_ERROR,
+												"%s lo saco de la lista  ", Leido_error);
+
+					pthread_exit(EXIT_FAILURE);//solo si se desconecta el planificador
+
+				} else {
+
+					log_in_disk_plan(LOG_LEVEL_TRACE,
+							"salida del segundo llamado %s nivel %s \n",
+							 buffer, h_planificador->desc_nivel);
+
+				}
 				free(buffer);
 
 			}
 		}
 	}
-	//cierro el socket que escucha para no aceptar nuevas conexiones.
-	//Como estoy en un while infinito no tiene sentido lo pogo como ejempo
+//cierro el socket que escucha para no aceptar nuevas conexiones.
+//Como estoy en un while infinito no tiene sentido lo pogo como ejempo
 
 	pthread_exit(EXIT_SUCCESS);
 }
 
+static void eliminar_nodo(int sck, t_list *list_planificadores) {
+	int index = 0;
+	t_h_planificador *h_planificador;
+
+	bool _list_elements(t_h_planificador *h_planificador) {
+
+		if (*(h_planificador->sock) == sck) {
+			log_in_disk_plat(LOG_LEVEL_INFO,
+					"Elimino el planificador %s de la lista",
+					h_planificador->desc_nivel);
+
+			return true;
+		}
+		index++;
+		return false;
+
+	}
+
+	if (list_find(list_planificadores, (void*) _list_elements)){
+		h_planificador = list_get(list_planificadores,index);
+		list_remove(list_planificadores, index);
+
+		free(h_planificador->desc_nivel);
+		free(h_planificador->sock);
+		free(h_planificador);
+	}
+}
--- plataforma/src/planificador/planificador_thr.h
+++ plataforma/src/planificador/planificador_thr.h
@@ -17,7 +17,16 @@ typedef struct {
 	char *desc_nivel; //nombre del nivel al que reprecenta el hilo
 	int *sock; //socket por el cual se comunica, es creado por la plataforma y pasado al planificador
 	fd_set *readfds; // puntero al fd_set para agregarlo en el select() dentro del hilo
-	fd_set *exceptfds;
+	char ip[16];
+	char puerto[6];
+	t_list *lista_planificadores; //tengo una referencia a los planificadores para sacarlo de la lista si es necesario
+	t_list *l_listos;
+	t_list *l_bloquedos;
+	t_list *l_errores;
+	pthread_mutex_t *s_lista_plani;
+	pthread_mutex_t *s_listos;
+	pthread_mutex_t *s_bloquedos;
+	pthread_mutex_t *s_errores;
 } t_h_planificador;
 
 typedef struct t_h_parlante{
