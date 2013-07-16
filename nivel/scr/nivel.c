2663eb156ee4e5ec2967e28de950a68cb8282c7c
Matias Chekherdemian <cheker85@gmail.com>
2013-07-16 08:49:43 -0300

    merge

--- nivel/scr/nivel.c
+++ nivel/scr/nivel.c
@@ -28,16 +28,17 @@
 #include <semaphore.h>
 #include <commons/collections/list.h>
 #include "manjo_pantalla/tad_items.h"
+#include "detecto_interbloque_th/detecto_interbloque_th.h"
 
 void add_personaje_lista(char id_personaje, char *nombre_personaje, int i,
 		t_h_personaje *t_personaje);
 t_lista_personaje *busco_personaje(int sck, t_list *l_personajes, int *i);
-struct h_t_recusos *busco_recurso(char id, t_list *recusos);
 void add_recurso_personaje(t_list *l_recursos_optenidos,
 		struct h_t_recusos *recurso_actual);
 void elimino_personaje_lista_nivel(int sck, t_list *l_personajes);
 void liberar_memoria(t_lista_personaje *personaje);
 void liberar_recursos(t_list *recursos_otenido);
+char * listarRecursosPersonaje(t_lista_personaje *personaje);
 
 //void libero_memoria(t_h_personaje *t_personaje, struct t_param_nivel *param_nivel);
 void sig_handler(int signo);
@@ -48,8 +49,9 @@ static pthread_mutex_t s_personaje_recursos = PTHREAD_MUTEX_INITIALIZER;
 int main(void) {
 	struct h_t_param_nivel param_nivel;
 	int sck_plat;
-	pthread_t escucho_personaje_th;
+	pthread_t escucho_personaje_th, detecto_interbloque_th;
 	t_h_personaje *t_personaje = malloc(sizeof(t_h_personaje));
+	t_h_interbloqueo h_interbloqueo;
 	fd_set readfds;
 	int i, tipo, rows, cols, posX, posY;
 	char *buffer;
@@ -60,8 +62,6 @@ int main(void) {
 	char *aux_mensaje;
 	struct h_t_recusos *recurso;
 	ITEM_NIVEL *ListaItems = NULL;
-	int iter;
-	bool seguir;
 	t_lista_personaje *nodo_lista_personaje;
 	int tipo_mensaje, catidad_recursos;
 	char *nombre_recurso;
@@ -98,6 +98,15 @@ int main(void) {
 	pthread_create(&escucho_personaje_th, NULL, (void*) escucho_personaje,
 			(void*) t_personaje);
 	pthread_mutex_lock(&s_personaje_conectado);
+
+
+	//setteo las estructura para pasar al hilo
+	memcpy(&h_interbloqueo.t_personaje,t_personaje,sizeof(t_h_personaje));
+	h_interbloqueo.param_nivel = param_nivel;
+	//creo el hilo para la deteccion de interbloqueo
+	pthread_create(&detecto_interbloque_th, NULL, (void*) detecto_interbloque,
+				(void*) &h_interbloqueo);
+
 	for (;;) {
 
 		if (select(t_personaje->sck_personaje + 1, t_personaje->readfds, NULL,
@@ -125,8 +134,7 @@ int main(void) {
 							cont_msj, mensaje[cont_msj]);
 
 				}
-				iter = 0;
-				seguir = true;
+
 
 				switch (tipo) {
 				case P_TO_N_UBIC_RECURSO:
@@ -150,22 +158,38 @@ int main(void) {
 					posX = atoi(mensaje[3]);
 					posY = atoi(mensaje[4]);
 					MoverPersonaje(ListaItems, mensaje[0][0], posX, posY);
+					//la informacion del la posicion la necesito para determinar el interbloqueo
+					pthread_mutex_lock(&s_personaje_recursos);
+					nodo_lista_personaje = busco_personaje(i,
+												t_personaje->l_personajes, &pos);
+					nodo_lista_personaje->posX = posX;
+					nodo_lista_personaje->posX = posY;
+					pthread_mutex_unlock(&s_personaje_recursos);
 					if (B_DIBUJAR) {
 						nivel_gui_dibujar(ListaItems);
 					}
+
+
 					aux_mensaje = "te movi";
 					fd_mensaje(i, N_TO_P_MOVIDO, aux_mensaje, &tot_enviados);
 
 					break;
+
+				case P_TO_N_OBJ_CUMPLIDO:
+
+									nodo_lista_personaje = busco_personaje(i,
+											t_personaje->l_personajes, &pos);
+									aux_mensaje = listarRecursosPersonaje(nodo_lista_personaje);
+									fd_mensaje(i, N_TO_O_PERSONAJE_TERMINO_NIVEL, aux_mensaje,
+											&tot_enviados);
+
+									break;
+
 				case P_TO_N_INICIAR_NIVEL:
 
 					personaje_pantalla(mensaje[1][0], 1, 1, &ListaItems);
 					add_personaje_lista(mensaje[1][0], mensaje[0], i,
 							t_personaje);
-
-					if (B_DIBUJAR) {
-						nivel_gui_dibujar(ListaItems);
-					}
 					break;
 
 				case P_TO_N_SOLIC_RECURSO:
@@ -196,10 +220,9 @@ int main(void) {
 								nodo_lista_personaje->proximo_recurso->SIMBOLO,
 								nodo_lista_personaje->l_recursos_optenidos);
 
-						restarRecurso(ListaItems, nodo_lista_personaje->proximo_recurso->SIMBOLO);
-						if (B_DIBUJAR) {
-							nivel_gui_dibujar(ListaItems);
-						}
+						restarRecurso(ListaItems,
+								nodo_lista_personaje->proximo_recurso->SIMBOLO);
+
 						sleep(10);
 						if (recurso != NULL ) { //agreo a la lista de recursos asignados al personaje
 							recurso->cantidad++; //si esta en la lista le agrego una instancia el recurso que ya tiene el personaje
@@ -230,6 +253,9 @@ int main(void) {
 					break;
 				}
 
+				if (B_DIBUJAR) {
+					nivel_gui_dibujar(ListaItems);
+				}
 				free(buffer);
 
 			}
@@ -285,7 +311,9 @@ void sig_handler(int signo) {
 	//nivel_gui_terminar();
 
 }
-
+/**
+ * el 3er parametro en la posicion en la lista donde se encontro el personaje
+ */
 t_lista_personaje *busco_personaje(int sck, t_list *l_personajes, int *i) {
 
 	t_lista_personaje *personaje;
@@ -306,27 +334,6 @@ t_lista_personaje *busco_personaje(int sck, t_list *l_personajes, int *i) {
 
 	}
 	return personaje;
-}
-/**
- * Busco el recurso por el id, y lo devuelvo en caso de no exister el recurso en la lista devuelvo null
- */
-struct h_t_recusos *busco_recurso(char id, t_list *recusos) {
-	int i, tot_elementos;
-	struct h_t_recusos *recurso = NULL;
-	struct h_t_recusos *recurso_aux = NULL;
-
-	log_in_disk_niv(LOG_LEVEL_TRACE, "Buesco el recurso %c", id);
-
-	tot_elementos = list_size(recusos);
-
-	for (i = 0; i < tot_elementos; i++) {
-		recurso_aux = (struct h_t_recusos*) list_get(recusos, i);
-
-		if (id == recurso_aux->SIMBOLO) {
-			recurso = recurso_aux;
-		}
-	}
-	return recurso;
 }
 
 /**
@@ -377,4 +384,33 @@ void liberar_recursos(t_list *recursos_otenido) {
 		recurso_aux->ref_recuso->cantidad = +recurso_aux->cantidad;
 	}
 	list_destroy(recursos_otenido);
+}
+
+char * listarRecursosPersonaje(t_lista_personaje * personaje) {
+
+	int cantidadRecu = list_size(personaje->l_recursos_optenidos);
+	int i;
+	int cantidadRecursos = 0;
+	//char* recursos = string_new() ; //hacer malloc y free
+	char recursos[250] = ("");
+	t_recusos *recurso_aux;
+	char* respuesta;
+	//int aux;
+
+	for (i = 0; i < cantidadRecu; i++) {
+		recurso_aux = list_get(personaje->l_recursos_optenidos, i);
+		cantidadRecursos = cantidadRecursos + recurso_aux->cantidad;
+		//string_append(char** original, char* string_to_add)
+		//strcat(recursos,";");
+		//string_append(&recursos, (char*) recurso_aux->SIMBOLO);
+		//aux = strlen(recursos);
+		for (i = 0; i < recurso_aux->cantidad; i++) {
+			strcat(recursos, recurso_aux->SIMBOLO);
+			strcat(recursos, ';');
+					}
+		respuesta = string_from_format("%d;%s", cantidadRecursos,
+				recursos);
+	}
+
+	return respuesta;
 }
