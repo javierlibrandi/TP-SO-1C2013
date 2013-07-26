8b5389f5baed410fa4acf4ba04d1b3800be6b7d2
Javier Librandi <javierlibrandi@gmail.com>
2013-07-26 18:08:08 -0300

    sumo los recuros a la pantalla cuando se liberan

--- nivel/scr/nivel.c
+++ nivel/scr/nivel.c
@@ -33,9 +33,9 @@
 t_lista_personaje *busco_personaje(int sck, t_list *l_personajes, int *i);
 void add_recurso_personaje(t_list *l_recursos_optenidos,
 		struct h_t_recusos *recurso_actual);
-void elimino_personaje_lista_nivel(int sck, t_list *l_personajes);
-void liberar_memoria(t_lista_personaje *personaje);
-void liberar_recursos(t_list *recursos_otenido);
+void elimino_personaje_lista_nivel(int sck, t_list *l_personajes,ITEM_NIVEL *item);
+void liberar_memoria(t_lista_personaje *personaje,ITEM_NIVEL *item);
+void liberar_recursos(t_list *recursos_otenido,ITEM_NIVEL *item);
 void listarRecursosPersonaje(t_list * lista_Recursos, char * recursos);
 t_lista_personaje *busca_personaje_simbolo(char id, t_list *l_personajes,
 		int *indice_personaje);
@@ -128,7 +128,7 @@ int main(void) {
 				buffer = recv_variable(i, &tipo); // *(t_h_orq->sock) Para mi es i el 1er parametro del rec por que el socket que me respondio tiene ese valor.
 				if (!strcmp(buffer, Leido_error)) {
 
-					elimino_personaje_lista_nivel(i, t_personaje->l_personajes);
+					elimino_personaje_lista_nivel(i, t_personaje->l_personajes,ListaItems);
 					elimino_sck_lista(i, t_personaje->readfds);
 
 				}
@@ -237,7 +237,7 @@ int main(void) {
 								mensaje);
 
 						elimino_personaje_lista_nivel(i,
-								t_personaje->l_personajes);
+								t_personaje->l_personajes,ListaItems);
 						//liberar_memoria(nodo_lista_personaje); // ya se libera la memoria adentro de la funcion anterior (elimino_personaje_lista_nivel)
 						elimino_sck_lista(i, t_personaje->readfds);
 						//TODO Actualizar los recursos en la pantalla sumando los que libero el personaje.
@@ -331,8 +331,10 @@ int main(void) {
 								ListaItems);
 					}
 					liberar_recursos(
-							nodo_lista_personaje->l_recursos_optenidos);
+							nodo_lista_personaje->l_recursos_optenidos,ListaItems);
 					pthread_mutex_unlock(&s_personaje_recursos);
+
+					nodo_lista_personaje->l_recursos_optenidos = list_create();
 
 					personaje_pantalla(mensaje[1][0], 1, 1, &ListaItems);
 					add_personaje_lista(mensaje[1][0], mensaje[0], i,
@@ -539,24 +541,24 @@ void add_recurso_personaje(t_list *l_recursos_optenidos,
 	list_add(l_recursos_optenidos, recurso);
 }
 
-void elimino_personaje_lista_nivel(int sck, t_list *l_personajes) {
+void elimino_personaje_lista_nivel(int sck, t_list *l_personajes,ITEM_NIVEL *item) {
 	int pos;
 
 	pthread_mutex_lock(&s_personaje_recursos);
 	t_lista_personaje *personaje;
 	busco_personaje(sck, l_personajes, &pos);
 	personaje = (t_lista_personaje *) list_remove(l_personajes, pos);
-	liberar_memoria(personaje);
+	liberar_memoria(personaje,item);
 	pthread_mutex_unlock(&s_personaje_recursos);
 }
 
-void liberar_memoria(t_lista_personaje *personaje) {
-	liberar_recursos(personaje->l_recursos_optenidos);
+void liberar_memoria(t_lista_personaje *personaje,ITEM_NIVEL *item) {
+	liberar_recursos(personaje->l_recursos_optenidos,item);
 	free(personaje->nombre_personaje);
 	free(personaje->proximo_recurso);
 }
 
-void liberar_recursos(t_list *recursos_otenido) {
+void liberar_recursos(t_list *recursos_otenido,ITEM_NIVEL *item) {
 	int total_recursos;
 	int i;
 	t_recusos *recurso_aux;
@@ -565,6 +567,12 @@ void liberar_recursos(t_list *recursos_otenido) {
 	for (i = 0; i < total_recursos; i++) {
 		recurso_aux = (t_recusos *) list_get(recursos_otenido, i);
 		recurso_aux->ref_recuso->cantidad = +recurso_aux->cantidad;
+
+		//si esta puesta la forma grafiaca y la tantidad de recursos es mayor o
+		//sumo recurosos a la pantalla
+		while(B_DIBUJAR && recurso_aux->cantidad--){
+			sumarRecurso(item,recurso_aux->SIMBOLO);
+		}
 	}
 	list_destroy(recursos_otenido);
 }
