9b007601242be28339e12c8ba599470b829abeb2
MATIAS CHEKHERDEMIAN <cheker85@gmail.com>
2013-07-26 15:51:17 -0300

    sacamos las referencas a libmario

--- plataforma/src/planificador/planificador_thr.c
+++ plataforma/src/planificador/planificador_thr.c
@@ -256,12 +256,13 @@ static void mover_personaje(t_personaje *personaje,
 			eliminar_personaje_termino_nivel(personaje->sck,
 					h_planificador->l_listos);
 
-			elimino_sck_lista(personaje->sck, h_planificador->readfds);
+			//elimino_sck_lista(personaje->sck, h_planificador->readfds);
 			imprimir_listas(h_planificador, 'p');
 			un_lock_listas_plataforma(h_planificador);
 
 			fd_mensaje(sock_aux, OK, "Me alegro pos vos!!!!", &byteEnviados);
 
+			close(personaje->sck);
 			personaje_bloqueado = true;
 
 			break;
