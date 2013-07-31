9d8d08f3c2b7073175dd5630615204c20ea13657
JazminSA <jazminacunia@gmail.com>
2013-07-31 15:25:14 -0300

    Se actualizan pendientes.

--- mario_para_todos/mario_para_todos/Probar.c
+++ mario_para_todos/mario_para_todos/Probar.c
@@ -7,22 +7,34 @@
 
 
 
-/* PROBAR
+/* PENDIENTES
 
-Reiniciar nivel
-reiniciar plan de niveles -> agregar salir del nivel P_TO_PL_SALIR, P_TO_N_SALIR
-perder quantum al conseguir un recurso
-modificar el quantum en tiempo de ejecución
-desbloqueo de personajes
-detección de interbloqueo
-elección personaje víctima
-muerte por deadlock (Perder una vida personaje)
-desconexión inesperada personajes/niveles
-gráficos, sumar recursos en pantalla
-integrar koopa
-restar recursos al desbloquear (nivel), ya que el personaje se los asigna automáticamente
-ingresar niveles en cualquier momento. Mepa q plataforma no reconoce niveles q entran despues del personaje
-limites del mapa nivel
+DESARROLLAR - TERMINAR
+*Nivel: Detección de interbloqueo
+*Nivel: Elección personaje víctima. Enviar PL_TO_P_MUERTE.
+*Plataforma/Orquestador: Integrar koopa
+*Hacer los archivos de configuración de niveles y personajes para probar el sábado todas las situaciones pedidas (y para probar nosotros xD).
+
+SOLUCIONAR
+*Nivel: Restar recursos al desbloquear, ya que el personaje se asigna automáticamente el recurso por el que había quedado bloqueado
+*Plataforma: Desconexión inesperada de personajes/niveles
+*Uso de memoria/consumo de CPU
+*Uso *Planificador: Modificar el quantum en tiempo de ejecución
+
+PROBAR
+*Nivel: función DESBLOQUEAR_PERSONAJES (utilizada en OBJ_CUMPLIDO, REINICIAR_NIVEL, SALIR)
+*Nivel: Gráficos en pantalla
+*Nivel: Restar y sumar recursos del nivel
+*Eliminar personaje del nivel y del planificador cuando muere (sacarlo del mapa/lista, liberar recursos, desbloquear, etc.) -> Case P_TO_PL_SALIR /P_TO_N_SALIR
+*Nivel: Reiniciar nivel(liberar recursos, desbloquear personajes, posicionar personaje, etc.) -> Case P_TO_N_REINICIAR_NIVEL
+*Personaje: Reiniciar nivel
+*Personaje: Reiniciar plan de niveles
+*Planificador: Perder quantum al conseguir un recurso
+*Recontraprobar desbloqueo de personajes
+*Personaje: Muerte por deadlock
+*Koopa
+*Compilación por consola, server de test, makefiles, etc.
+
  */
 
 
